5ed5bc15ffd64a8b2ed5b3aacbc7fc86b45dc469
Eduardo Pardo <doinglight@Eduardos-iMac.local>
2026-03-25 09:19:11 +0100

    Fix EAS config and PDF assets

--- metro.config.js
+++ metro.config.js
@@ -5,5 +5,6 @@ const config = getDefaultConfig(__dirname);
 // In this environment native `find` crawling can hang and leave Metro unresponsive.
 // Disabling watchman forces the Node filesystem crawler path.
 config.resolver.useWatchman = false;
+config.resolver.assetExts = [...config.resolver.assetExts, 'pdf'];
 
 module.exports = config;
